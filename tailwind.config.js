eab733117760affa325c2db6ef13287d85022930
Rahimov777 <rahimovyusuf10@gmail.com>
2025-04-28 16:42:06 +0500

    PRAKTIKA-2-KASA

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend:{
+      screens: {
+        'sm': '0px',
+
+  
+        'md': '510px',
+
+  
+        'lg': '1024px',
+
+  
+        'xl': '1280px',
+
+  
+        '2xl': '1536px',
+
+      }
+    } ,
+  },
+  plugins: [],
+}
